41f1e235d03090146a750a6a6a0382093aaa8ad9
giekaton <git@giekaton.com>
2023-01-01 15:40:30 +0200

    Footer update

--- resources/js/partials/Footer.vue
+++ resources/js/partials/Footer.vue
@@ -3,7 +3,7 @@
     <div class="width">
 
     <div style="float:left;margin-top:1px;">
-      <span @click="backHome()" style="cursor:pointer;">Set in Block 2018-2022</span>
+      <span @click="backHome()" style="cursor:pointer;">Set in Block 2018-2023</span>
     </div>
 
     <div class="sans-serif" style="float:right;color:#777;margin-top:-2px;">
